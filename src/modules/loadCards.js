97aefe6559d8adf6a81e047885e046d1a5a343be
fasaldana <fernando_s_98@hotmail.com>
2022-04-13 15:15:12 -0500

    Fixed linters

--- src/modules/loadCards.js
+++ src/modules/loadCards.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const ulCards = document.querySelector('.home-list');
 
-const getData = async (id) => {
+const getData = async () => {
   try {
     for (let i = 0; i < 9; i += 1) {
-      const res = await axios(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`);
+      const res = axios(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`);
       console.log(res.data);
       const liCard = document.createElement('li');
       liCard.innerHTML = `
