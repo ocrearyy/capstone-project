085c28f5fb0ae6935f5efe8df0f942d17b11871b
fasaldana <fernando_s_98@hotmail.com>
2022-04-13 17:07:50 -0500

    Fixed clean code

--- src/modules/loadCards.js
+++ src/modules/loadCards.js
@@ -6,24 +6,27 @@ const getData = async () => {
   try {
     for (let i = 0; i < 9; i += 1) {
       const res = await axios(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`);
-      console.log(res.data);
-      const liCard = document.createElement('li');
-      liCard.innerHTML = `
-        <img src="${res.data.sprites.other.dream_world.front_default}" alt="pokemon image" class="pkm-img">
-        <div class="card-title">
-            <h3>${res.data.forms[0].name}</h3>
-            <div class="card-likes">
-                <i class="fa-regular fa-heart"></i>
-                <p>5 likes</p>
-            </div>
-        </div>
-        <button class="comment-btn">Comments</button>
-        `;
-      ulCards.appendChild(liCard);
+      loadCards(res);
     }
   } catch (error) {
     console.log(error);
   }
+};
+
+const loadCards = (res) => {
+  const liCard = document.createElement('li');
+  liCard.innerHTML = `
+          <img src="${res.data.sprites.other.dream_world.front_default}" alt="pokemon image" class="pkm-img">
+          <div class="card-title">
+              <h3>${res.data.forms[0].name}</h3>
+              <div class="card-likes">
+                  <i class="fa-regular fa-heart"></i>
+                  <p>5 likes</p>
+              </div>
+          </div>
+          <button class="comment-btn">Comments</button>
+          `;
+  ulCards.appendChild(liCard);
 };
 
 export default getData;
