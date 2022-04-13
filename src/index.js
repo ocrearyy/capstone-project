d18f2f9efdd41bcaa34fb853df1056ffcfc2e2a1
fasaldana <fernando_s_98@hotmail.com>
2022-04-13 13:54:18 -0500

    Added header and display list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+import loadCards from './modules/loadCards';
+
+loadCards();
---
+++ src/modules/loadCards.js
@@ -0,0 +1,31 @@
+const getData = async (id) => {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    const data = res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const loadCards = () => {
+  const ulCards = document.querySelector('.home-list');
+  for (let i = 0; i < 6; i += 1) {
+    const liCard = document.createElement('li');
+    liCard.innerHTML = `
+    <img src="" alt="pokemon image" class="pkm-img">
+    <div class="card-title">
+        <h3>Name</h3>
+        <div class="card-likes">
+            <i class="fa-regular fa-heart"></i>
+            <p>5 likes</p>
+        </div>
+    </div>
+    <button class="comment-btn">Comments</button>
+    `;
+    ulCards.appendChild(liCard);
+  }
+};
+
+export default loadCards;
