06e0079c4fce20cee0d2cc22d38af535e4bddd8f
fasaldana <fernando_s_98@hotmail.com>
2022-04-13 14:18:08 -0500

    Fixed linters errors

--- src/modules/loadCards.js
+++ src/modules/loadCards.js
@@ -1,19 +1,9 @@
-const getData = async (id) => {
-  try {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/9/`);
-    const data = res.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const loadCards = () => {
   const ulCards = document.querySelector('.home-list');
   for (let i = 0; i < 6; i += 1) {
     const liCard = document.createElement('li');
     liCard.innerHTML = `
-    <img src="" alt="pokemon image" class="pkm-img">
+    <img src=" " alt="pokemon image" class="pkm-img">
     <div class="card-title">
         <h3>Name</h3>
         <div class="card-likes">
