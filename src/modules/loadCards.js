6a3904ef40b376073d357600ae966bb79c6657b5
fasaldana <fernando_s_98@hotmail.com>
2022-04-14 18:33:59 -0500

    fixed functions for get data

--- src/modules/loadCards.js
+++ src/modules/loadCards.js
@@ -1,37 +1,47 @@
 import axios from 'axios';
 
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
 const ulCards = document.querySelector('.home-list');
-
-const getData = async () => {
-  try {
-    for (let i = 0; i < 9; i += 1) {
-      const res = await axios(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`);
-      const res1 = await axios(
-        `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NPu1KWpwmlTnYdtWyYwl/likes/`
-      );
-      console.log(res1.data[i].item_id);
-      if (res1.data[i].item_id === res.data.forms[0].name)
-        loadCards(res, res1.data[i]);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
+const results = [];
 
 const loadCards = (res, resLikes) => {
   const liCard = document.createElement('li');
   liCard.innerHTML = `
-          <img src="${res.data.sprites.other.dream_world.front_default}" alt="pokemon image" class="pkm-img">
-          <div class="card-title">
-              <h3>${res.data.forms[0].name}</h3>
-              <div class="card-likes">
-                  <i class="fa-regular fa-heart"></i>
-                  <p>${resLikes.likes} likes</p>
-              </div>
-          </div>
-          <button class="comment-btn">Comments</button>
-          `;
+            <img src="${res.data.sprites.other.dream_world.front_default}" alt="pokemon image" class="pkm-img">
+            <div class="card-title">
+                <h3>${res.data.forms[0].name}</h3>
+                <div class="card-likes">
+                    <i class="fa-regular fa-heart"></i>
+                    <p>${resLikes.likes} likes</p>
+                </div>
+            </div>
+            <button class="comment-btn">Comments</button>
+            `;
   ulCards.appendChild(liCard);
+};
+
+const renderData = (res1) => {
+  for (let i = 0; i < 9; i += 1) {
+    if (res1.data[i].item_id === results[i].data.forms[0].name) {
+      loadCards(results[i], res1.data[i]);
+    }
+  }
+};
+
+const getData = async (i) => {
+  try {
+    const res = await axios(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`);
+    results.push(res);
+    if (i === 8) {
+      const res1 = await axios(`${url}/apps/NPu1KWpwmlTnYdtWyYwl/likes/`);
+      renderData(res1);
+    } else {
+      i += 1;
+      getData(i);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default getData;
