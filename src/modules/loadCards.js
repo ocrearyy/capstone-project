e0db2a46d09fe552242e5b8b61a9fa445d008b6c
fasaldana <fernando_s_98@hotmail.com>
2022-04-14 11:07:17 -0500

    Showed likes for every item

--- src/modules/loadCards.js
+++ src/modules/loadCards.js
@@ -6,14 +6,19 @@ const getData = async () => {
   try {
     for (let i = 0; i < 9; i += 1) {
       const res = await axios(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`);
-      loadCards(res);
+      const res1 = await axios(
+        `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NPu1KWpwmlTnYdtWyYwl/likes/`
+      );
+      console.log(res1.data[i].item_id);
+      if (res1.data[i].item_id === res.data.forms[0].name)
+        loadCards(res, res1.data[i]);
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-const loadCards = (res) => {
+const loadCards = (res, resLikes) => {
   const liCard = document.createElement('li');
   liCard.innerHTML = `
           <img src="${res.data.sprites.other.dream_world.front_default}" alt="pokemon image" class="pkm-img">
@@ -21,7 +26,7 @@ const loadCards = (res) => {
               <h3>${res.data.forms[0].name}</h3>
               <div class="card-likes">
                   <i class="fa-regular fa-heart"></i>
-                  <p>5 likes</p>
+                  <p>${resLikes.likes} likes</p>
               </div>
           </div>
           <button class="comment-btn">Comments</button>
