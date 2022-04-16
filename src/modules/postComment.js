807f696c84cc557ca4230dc9075e61569d066099
fasaldana <fernando_s_98@hotmail.com>
2022-04-16 14:23:16 -0500

    Fixed lintesr

--- src/modules/postComment.js
+++ src/modules/postComment.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const postComment = (item_id, username, comment) => {
+const postComment = (id, username, comment) => {
   axios
     .post(
       'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NPu1KWpwmlTnYdtWyYwl/comments',
-      { item_id, username, comment }
+      { item_id: id, username, comment },
     )
     .catch((error) => {
       console.log(error);
