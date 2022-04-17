75121ceb5e98fae12a523f8113e2bc299951d387
fasaldana <fernando_s_98@hotmail.com>
2022-04-16 19:08:54 -0500

    fixed linters

--- src/modules/postComment.js
+++ src/modules/postComment.js
@@ -4,7 +4,7 @@ const postComment = (id, username, comment) => {
   axios
     .post(
       'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NPu1KWpwmlTnYdtWyYwl/comments',
-      { item_id: id, username, comment }
+      { item_id: id, username, comment },
     )
     .catch((error) => {
       console.log(error);
